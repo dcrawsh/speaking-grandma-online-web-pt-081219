83b88880134e10827ac774ded51cf47afa3960a5
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-15 18:57:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
   elsif phrase == 'WHAT DID YOU EAT TODAY?'
+    return "NO, NOT SINCE 1938!"
   else
     return 'HUH?! SPEAK UP, SONNY!'
   
